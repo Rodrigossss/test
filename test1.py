0b8ec11c42d779433a852c96a55797bce6d33700
ibliyhsb <catd.rojas@duocuc.cl>
2024-06-27 16:13:13 -0400

    final

--- test1.py
+++ test1.py
@@ -1,14 +1,28 @@
 
 import csv
-notas=[]
-def calcular_np(notas):
-    for nota in notas:
-        np = (nota*0.30)+(nota*0.35)+(nota*0.35)
-        return print(np)
+p1=[]
+p2=[]
+p3=[]
+asistencia=[]
+
+def calcular_np(p1, p2, p3):
+    for nota in p1:
+        nota= float(nota)
+        n1 = (nota*0.30)
+    for nota in p2:
+        nota= float(nota)
+        n2 = (nota*0.35)
+    for nota in p3:
+        nota= float(nota)
+        n3 = (nota*0.35)
+    np=(n1+n2+n3)
+    return print(np)
+        
 
 def ri(asistencia):
-    for dia in asistencia:
-        promedio=(dia+dia+dia+dia+dia+dia+dia)/7
+    for dia in (asistencia):
+        dia=int(dia)
+        promedio=(dia+dia+dia+dia+dia)/5
         if promedio>=0.65:
             return print("aprobó")
         else:
@@ -23,7 +37,17 @@ def ri(asistencia):
 with open ('estudiantes_archivo.csv', 'r', newline='') as estudiantes_archivo_csv:
     reader= csv.reader(estudiantes_archivo_csv)
     for fila in reader:
-        for columna in fila[1]:
-            notas.append(columna)
+        p1.append(fila[1])
+        p2.append(fila[2])
+        p3.append(fila[3])
+        p4=p1+p2+p3
+        calcular_np(p1, p2, p3)
+        asistencia.append(fila[4])
+        asistencia.append(fila[5])
+        asistencia.append(fila[6])
+        asistencia.append(fila[7])
+        asistencia.append(fila[8])
+        ri(asistencia)
 
-print(notas)
+print(p1, p2, p3)
+print(asistencia)
